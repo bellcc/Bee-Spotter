d868537471142814402f3bc61194b1c55e154dd4
chris <compute-bot@protonmail.com>
2018-04-28 17:48:12 -0400

    google maps

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -5,6 +5,8 @@ import { Geolocation } from '@ionic-native/geolocation';
 import {GoogleMaps, GoogleMap, GoogleMapsEvent, Marker, LatLng} from '@ionic-native/google-maps';
 
 import { Platform } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
 
 @IonicPage()
 @Component({
@@ -16,7 +18,8 @@ export class LocationPage {
   map: GoogleMap;
   spotting: {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private platform: Platform, private geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private platform: Platform,
+              private geolocation: Geolocation, private toastCtrl: ToastController) {
     platform.ready()
       .then(() => {
         geolocation.getCurrentPosition().then(pos => {
@@ -28,14 +31,13 @@ export class LocationPage {
 
   ionViewDidLoad() {
     this.loadMap();
+    this.presentToast("Zoom in on the location of the bee spotting. Add a marker to the map. Drag marker to correct location.");
   }
 
   private loadMap(): void {
     this.map = GoogleMaps.create('map_canvas');
 
-    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
-      // this.createMarker(39.515155, -84.744968);
-    });
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {});
   }
 
   private createMarker(latitude: number, longitude: number) {
@@ -62,9 +64,25 @@ export class LocationPage {
   public markLocation(): void {
     let pos = this.map.getCameraPosition();
     this.createMarker(pos.target.lat, pos.target.lng);
+    this.presentToast(pos.target.lat + ", " + pos.target.lng);
   }
 
   public getSpottingLocation(): object {
     return this.spotting;
+  }
+
+  private presentToast(message): void {
+    let toast = this.toastCtrl.create({
+      message: message,
+      position: 'bottom',
+      closeButtonText: 'Close',
+      showCloseButton: true
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 }
